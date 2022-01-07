bf3fd88fac4a3169ede3f2e63b4e6dce5833b35d
10Yatin <97273373+10Yatin@users.noreply.github.com>
2022-01-07 11:01:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 <div class="container">
 <form name="login" action="logged.php" method="post">
 <input type="text" name="user" id="txtUser"><br/>
-<input type="password" name="pwd" id="txtPwd">
+<input type="password" name="pwd" id="txtPwd"><br/>
+<input type="submit" value="login">
+</form>
 </div>
 </body>
 </html>
