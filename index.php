8400a1c7962288818cfe4fde13f247d0d975b31d
10Yatin <97273373+10Yatin@users.noreply.github.com>
2022-01-07 10:59:21 +0530

    verify.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE Html>
+<html>
+<body>
+<div class="container">
+<form name="login" action="logged.php" method="post">
+<input type="text" name="user" id="txtUser"><br/>
+<input type="password" name="pwd" id="txtPwd">
+</div>
+</body>
+</html>
